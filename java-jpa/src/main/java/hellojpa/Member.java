a07998b13a59e0d6743499d37409190441cca781
inkyu <RndurRndur1!>
2021-11-27 23:37:29 +0900

    일대다 [1:N]

--- java-jpa/src/main/java/hellojpa/Member.java
+++ java-jpa/src/main/java/hellojpa/Member.java
@@ -13,7 +13,7 @@ public class Member {
     private String username;
 
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
     public Long getId() {
@@ -30,14 +30,6 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
     }
 
 }
