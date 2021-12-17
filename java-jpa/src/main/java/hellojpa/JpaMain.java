bf8cf53ac42ed21779bfac7d1dac1f6e248f62f9
inkyu <RndurRndur1!>
2021-12-17 22:57:33 +0900

    즉시 로딩과 지연 로딩

--- java-jpa/src/main/java/hellojpa/JpaMain.java
+++ java-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,18 +19,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            Hibernate.initialize(refMember); //강제초기화
+            Member m = em.find(Member.class, member1.getId());
 
+            System.out.println("m = " + m.getTeam().getClass());
 
+            System.out.println("==============================");
+            System.out.println("teamName = " + m.getTeam().getName());
+            System.out.println("==============================");
 
             tx.commit();
         } catch (Exception e) {
