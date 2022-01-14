36846acf2354a508d02d1df79dca5fa82e6a38bc
inkyu <inkyu192@gmail.com>
2022-01-14 13:05:13 +0900

    실전 예제 6 - 값 타입 매핑

--- java-jpa/src/main/java/hellojpa/Address.java
+++ java-jpa/src/main/java/hellojpa/Address.java
@@ -23,7 +23,7 @@ public class Address {
         return city;
     }
 
-    private void setCity(String city) {
+    public void setCity(String city) {
         this.city = city;
     }
 
@@ -31,7 +31,7 @@ public class Address {
         return street;
     }
 
-    private void setStreet(String street) {
+    public void setStreet(String street) {
         this.street = street;
     }
 
@@ -39,7 +39,7 @@ public class Address {
         return zipcode;
     }
 
-    private void setZipcode(String zipcode) {
+    public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 
